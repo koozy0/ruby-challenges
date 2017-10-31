97b4458a323176a230ac4b90d752c9e9855cdd57
koozy <koozhengyu@gmail.com>
2017-10-31 17:45:08 +0800

    q1

---
+++ lib/q1.rb
@@ -0,0 +1,21 @@
+puts "What calculation would you like to do? (add, sub, mult, div)"
+operation = gets.chomp
+puts "What is number 1?"
+num_one = gets.chomp.to_i
+puts "What is number 2?"
+num_two = gets.chomp.to_i
+
+case operation
+when "add"
+  sum = num_one + num_two
+  p "Your result is #{sum}"
+when "sub"
+  sum = num_one - num_two
+  p "Your result is #{sum}"
+when "mult"
+  sum = num_one * num_two
+  p "Your result is #{sum}"
+when "div"
+  sum = num_one / num_two
+  p "Your result is #{sum}"
+end
